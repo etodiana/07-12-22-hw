6bcbf6753d65752455f3c4e59b17bba9746f0a0e
etodiana <116962414+etodiana@users.noreply.github.com>
2022-12-07 10:32:36 +0300

    Add files via upload
    табло

---
+++ main (5).py
@@ -0,0 +1,42 @@
+n = int(input())
+
+m = int(input())
+
+advert = []
+
+for i in range (n):
+    
+for j in range (m):
+        
+input(advert)
+
+
+pixel = '.'
+
+p = []
+
+for i in range (n):
+    
+for j in range (m):
+        
+p = int(input())
+        
+if p > 3:
+            
+pixel += 'R'
+        
+if p % 2 == 1:
+            
+pixel += 'B'
+        
+if p == 2 or p == 3 or p == 6 or p ==7:
+            
+pixel += 'G'
+        
+if p [i][j] == advert [i][j]:
+            
+print ('YES')
+        
+else:
+            
+print ('NO')
