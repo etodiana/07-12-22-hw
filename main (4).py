b33548c8a3cdc079dfca18bdb9a2dd6f970ae781
etodiana <116962414+etodiana@users.noreply.github.com>
2022-12-07 10:30:40 +0300

    Add files via upload
    проверка на симпатичность

---
+++ main (4).py
@@ -0,0 +1,27 @@
+a = []
+
+t = int(input())
+
+for x in range (t):
+    
+m = int(input())
+    
+n = int(input())
+    
+for i in range (m):
+        
+for j in range (n):
+            
+input(a)
+
+for i in range (m):
+    
+for j in range (n):
+        
+if a[i][j] == a[i][j+1] == a[i+1][j] == a[i+1][j+1]:
+            
+print('NO')
+        
+else:
+            
+print ('YES')
